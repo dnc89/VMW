8d2ce53d8b7d07b0f18a8c562eb4ca049d2d521e
choudhuryd <m.dwarika89@gmail.com>
2023-09-14 11:29:41 +0000

    updated script

--- maxlengthsubstring.py
+++ maxlengthsubstring.py
@@ -1,3 +1,21 @@
 """
 
 """
+# Sample list of strings
+strings = ["Hello, world!", "This is a longer string", "Short one", "Maximum length string here"]
+
+# Initialize a variable to store the maximum length
+max_length = 0
+
+# Iterate through the strings in the list
+for s in strings:
+    # Check the length of the current string
+    current_length = len(s)
+
+    # Update max_length if the current string is longer
+    if current_length > max_length:
+        max_length = current_length
+
+# Print the maximum length
+print("Maximum length of strings:", max_length)
+
